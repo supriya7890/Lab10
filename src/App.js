160d66eeccea82116be15c3c1171a39280fb6b4b
supriya7890 <123706757+supriya7890@users.noreply.github.com>
2023-03-26 00:14:37 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Container from './components/Container';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Container />
+      </div>
+    </>
+  );
+}
+
+export default App;
